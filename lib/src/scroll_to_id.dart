55d49679c3daf683876cdc979c1f124ef2f691e8
Yusuke.Inoue <inoue@hatchout.net>
2020-12-14 15:23:35 +0900

    support web, dartfmt

--- lib/src/scroll_to_id.dart
+++ lib/src/scroll_to_id.dart
@@ -2,47 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:scroll_to_id/src/scroll_content_with_key.dart';
 
 class ScrollToId {
-
   ScrollController scrollController = ScrollController();
   List<ScrollContentWithKey> scrollContentsList = [];
 
   /// This function is to scroll to the widget that holds id.
   /// id is a required parameter that defines a default position to scroll.
   void scroll({String id}) {
-
     /// Scroll Target Height
     double sumHeight = 0;
 
-    for(ScrollContentWithKey scrollContents in scrollContentsList) {
-      if(scrollContents.id == id) {
+    for (ScrollContentWithKey scrollContents in scrollContentsList) {
+      if (scrollContents.id == id) {
         try {
-          if(sumHeight < scrollController.position.maxScrollExtent) {
+          if (sumHeight < scrollController.position.maxScrollExtent) {
             /// Scroll to id position
-            scrollController.animateTo(sumHeight, duration: Duration(milliseconds: 500), curve: Curves.ease);
+            scrollController.animateTo(sumHeight,
+                duration: Duration(milliseconds: 500), curve: Curves.ease);
             break;
-
           } else {
             /// Case Scrollable range is exceeded, Scroll to MaxScrollExtent
-            scrollController.animateTo(scrollController.position.maxScrollExtent, duration: Duration(milliseconds: 500), curve: Curves.ease);
+            scrollController.animateTo(
+                scrollController.position.maxScrollExtent,
+                duration: Duration(milliseconds: 500),
+                curve: Curves.ease);
             break;
-
           }
-
-        } catch(e) {
+        } catch (e) {
           print('$e');
           break;
         }
-
       } else {
         try {
           /// update Scroll target height
           sumHeight += scrollContents.key.currentContext.size.height;
-
-        } catch(e) {
+        } catch (e) {
           print('$e');
         }
       }
     }
   }
-
 }
